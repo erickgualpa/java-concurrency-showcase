84078943ee3b66f46139924692bce7be8e2e7758
Erick Gualpa <m.erickgualpa@gmail.com>
2025-03-08 19:20:48 +0100

    Update first test

--- executor-framework/src/test/java/org/egualpam/executorframework/ExecutorFrameworkTest.java
+++ executor-framework/src/test/java/org/egualpam/executorframework/ExecutorFrameworkTest.java
@@ -1,13 +1,11 @@
 package org.egualpam.executorframework;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
 import org.junit.jupiter.api.Test;
 
 public class ExecutorFrameworkTest {
 
   @Test
-  public void shouldAnswerWithTrue() {
-    assertTrue(true);
+  public void mainRuns() {
+    ExecutorFramework.main(new String[] {});
   }
 }

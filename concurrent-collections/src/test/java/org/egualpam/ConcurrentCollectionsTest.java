1e270e0382b8f233086b21976a31ca41c6cc69e3
Erick Gualpa <m.erickgualpa@gmail.com>
2025-03-16 16:41:07 +0100

    Setup concurrent-collections project

---
+++ concurrent-collections/src/test/java/org/egualpam/ConcurrentCollectionsTest.java
@@ -0,0 +1,12 @@
+package org.egualpam;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+class ConcurrentCollectionsTest {
+  @Test
+  public void shouldAnswerWithTrue() {
+    assertTrue(true);
+  }
+}
